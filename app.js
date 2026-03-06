7a175e5125f3830319da94c55b59962a6310f34e
Luca Giampieri <luca.giampieri95@gmail.com>
2026-03-06 16:35:37 +0100

    import cors, e express.static, creazione rotta home

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+//import middelware CORS
+const cors = require("cors");
 
+//attivazione della cartella public per uso file statici
+app.use(express.static('public'));
 
-
-
+//rotta home APP
+app.get('/api', (req, res) => {
+    res.send("<h1>Questa sarà la HomePage della pagina</h1>")
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
