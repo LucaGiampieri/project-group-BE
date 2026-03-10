a715ac70f6e9ed4fe5de30105a6beaa73414e764
Mattia Mion <mattia.mion@gmail.com>
2026-03-10 15:47:38 +0100

    Revert "Merge branch 'ramo-di-mattia'"
    This reverts commit 30ccb6c9ebd7aaac08700a2f57fd2671d5291caf, reversing
    changes made to 0a0c6ffaa28764bd880d77be25f82e44dcc72587.

--- middlewares/imagePath.js
+++
@@ -1,9 +0,0 @@
-//middlewares/imagePath.js
-function imagePath(folder) {
-    return (req, res, next) => {
-        req.imagePath = `${req.protocol}://${req.get('host')}/images/${folder}/`;
-        next();
-    };
-}
-
-module.exports = imagePath;
--- routers/regionRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-//rotta di index per regioni
-router.get('/', productController.indexRegions);
-
-module.exports = router;
